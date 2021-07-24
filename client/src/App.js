4da527766f0e9fa5ea9ab9c04caca2d746207b43
Quiahn <quiahnb@gmail.com>
2021-07-24 18:11:51 -0400

    Install mongodb
    Add mongodb to server
    Clean Up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,43 +4,8 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 function App() {
-  // Variables
-  const newTime = () => new Date().toLocaleTimeString()
-  // States
-  const [time, setTime] = useState(newTime)
-  const [disChicken, setDisChicken] = useState()
-  const [btnToggle, setBtnToggle] = useState(false)
+    // Variables
 
-  useEffect(() => {
-    setTimeout(() => {
-      setTime(newTime)
-    }, 1000)
-  }, [time])
-
-  useEffect(() => {
-    console.log('useEffect')
-    // get chicken GET request
-    axios.get("http://localhost:3001/apple")
-      // get chicken then
-      .then((res) => {
-        console.log("response: " + res.data.orange)
-        setDisChicken(res.data.orange)
-      })
-      // get chicken catch
-      .catch((err) => {
-        console.log("error: " + err)
-      })
-  }, [])
-
-  return (
-    <div className={"App " + (btnToggle ? 'red' : '')}>
-      <h1>Title</h1>
-      <Para />
-      <h3>Time is: {time}</h3>
-      <p>{disChicken}</p>
-      <button onClick={() => setBtnToggle(!btnToggle)}>{btnToggle.toString()}</button>
-    </div>
-  );
 }
 
 
--- server/app.js
+++ server/app.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const cors = require('cors')
-// Execute package
+const { MongoClient } = require('mongodb')
+// Variables
+const uri = "mongodb+srv://<username>:<password>@cluster0.otqkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 app.use(cors())
 
-// Routes
-app.get("/", function (req, res) {
-  // console.log("this is running");
-  // res.send("<h1></h1>");
-});
+// Connecting to mongodb
+async function connect() {
+	await client.connect()
+	console.log('Connected successfully')
+	const db = client.db('shopping-list')
+	const collection = db.collection('user')
 
-app.get("/apple", function (req, res) {
-  console.log('Something')
-  res.send({
-    orange: "chicken",
-  })
-})
+	return 'done.'
+}
+
+connect()
 // How do we start listening to the server
 app.listen(3001)
